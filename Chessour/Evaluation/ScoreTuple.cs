b416897f423ab00caf105fd6118a24babb72277d
İkbal Yaman <seogsei@gmail.com>
2023-12-09 01:06:52 +0300

    Using primary constructor and, adding multiply and division operators.

--- Chessour/Evaluation/ScoreTuple.cs
+++ Chessour/Evaluation/ScoreTuple.cs
@@ -2,21 +2,37 @@
 
 namespace Chessour.Evaluation
 {
-    public readonly struct ScoreTuple : IAdditionOperators<ScoreTuple, ScoreTuple, ScoreTuple>, IUnaryNegationOperators<ScoreTuple, ScoreTuple>
+    public readonly record struct ScoreTuple(int MidGame, int EndGame) 
+        : IAdditionOperators<ScoreTuple, ScoreTuple, ScoreTuple>,
+        IUnaryNegationOperators<ScoreTuple, ScoreTuple>,
+        IMultiplyOperators<ScoreTuple, int, ScoreTuple>,
+        IDivisionOperators<ScoreTuple, int, ScoreTuple>
     {
-        public ScoreTuple(int midGame, int endGame)
+        public static ScoreTuple Zero { get; } = new();
+
+        public static ScoreTuple operator +(ScoreTuple left, ScoreTuple right)
         {
-            MidGame = midGame;
-            EndGame = endGame;
+            return new(left.MidGame + right.MidGame, left.EndGame + right.EndGame);
         }
 
-        public readonly int MidGame { get; init; }
-        public readonly int EndGame { get; init; }
+        public static ScoreTuple operator -(ScoreTuple left, ScoreTuple right)
+        {
+            return new(left.MidGame - right.MidGame, left.EndGame - right.EndGame);
+        }
 
-        public static ScoreTuple Zero => default;
+        public static ScoreTuple operator -(ScoreTuple value)
+        {
+            return new(-value.MidGame, -value.EndGame);
+        }
 
-        public static ScoreTuple operator +(ScoreTuple left, ScoreTuple right) => new(left.MidGame + right.MidGame, left.EndGame + right.EndGame);
-        public static ScoreTuple operator -(ScoreTuple left, ScoreTuple right) => new(left.MidGame - right.MidGame, left.EndGame - right.EndGame);
-        public static ScoreTuple operator -(ScoreTuple value) => new(-value.MidGame, -value.EndGame);
+        public static ScoreTuple operator *(ScoreTuple left, int right)
+        {
+            return new(left.MidGame * right, left.EndGame * right);
+        }
+
+        public static ScoreTuple operator /(ScoreTuple left, int right)
+        {
+            return new(left.MidGame / right, left.EndGame / right);
+        }
     }
 }
