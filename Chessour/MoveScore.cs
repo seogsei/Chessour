e1ad69407dea6ad499be58cad480737525caaa97
İkbal Yaman <seogsei@gmail.com>
2023-12-09 12:40:50 +0300

    MoveScore type modernization.

--- Chessour/MoveScore.cs
+++ Chessour/MoveScore.cs
@@ -2,45 +2,81 @@
 
 namespace Chessour.Search
 {
-    public struct MoveScore : 
-        IComparisonOperators<MoveScore, MoveScore, bool>, IComparisonOperators<MoveScore, int, bool>
+    public struct MoveScore (Move move, int score = 0): 
+        IComparisonOperators<MoveScore, MoveScore, bool>,
+        IComparisonOperators<MoveScore, int, bool>
     {
-        public MoveScore(Move move)
+        public Move Move { get; set; } = move;
+        public int Score { get; set; } = score;
+ 
+        public static implicit operator MoveScore(Move move)
         {
-            Move = move;
-            Score = default;
+            return new(move);
         }
 
-        public MoveScore(Move move, int score)
+        public static implicit operator Move(MoveScore moveScore)
         {
-            Move = move;
-            Score = score;
+            return moveScore.Move;
         }
 
-        public Move Move { get; }
-        public int Score { get; set; }
+        public static bool operator >(MoveScore left, MoveScore right)
+        {
+            return left.Score > right.Score;
+        }
 
-        public static implicit operator MoveScore(Move move) => new(move);
+        public static bool operator >=(MoveScore left, MoveScore right)
+        {
+            return left.Score >= right.Score;
+        }
 
-        public static implicit operator Move(MoveScore moveScore) => moveScore.Move;
+        public static bool operator <(MoveScore left, MoveScore right)
+        {
+            return left.Score < right.Score;
+        }
 
-        public static bool operator >(MoveScore left, MoveScore right) => left.Score > right.Score;
-        public static bool operator >=(MoveScore left, MoveScore right) => left.Score >= right.Score;
-        public static bool operator <(MoveScore left, MoveScore right) => left.Score < right.Score;
-        public static bool operator <=(MoveScore left, MoveScore right) => left.Score <= right.Score;
+        public static bool operator <=(MoveScore left, MoveScore right)
+        {
+            return left.Score <= right.Score;
+        }
 
-        static bool IEqualityOperators<MoveScore, MoveScore, bool>.operator ==(MoveScore left, MoveScore right) => left.Score == right.Score;
-        static bool IEqualityOperators<MoveScore, MoveScore, bool>.operator !=(MoveScore left, MoveScore right) => left.Score != right.Score;
+        static bool IEqualityOperators<MoveScore, MoveScore, bool>.operator ==(MoveScore left, MoveScore right)
+        {
+            return left.Score == right.Score;
+        }
 
-        public static bool operator >(MoveScore left, int right) => left.Score > right;
+        static bool IEqualityOperators<MoveScore, MoveScore, bool>.operator !=(MoveScore left, MoveScore right)
+        {
+            return left.Score != right.Score;
+        }
 
-        public static bool operator >=(MoveScore left, int right) => left.Score >= right;
+        public static bool operator >(MoveScore left, int right)
+        {
+            return left.Score > right;
+        }
 
-        public static bool operator <(MoveScore left, int right) => left.Score < right;
+        public static bool operator >=(MoveScore left, int right)
+        {
+            return left.Score >= right;
+        }
 
-        public static bool operator <=(MoveScore left, int right) => left.Score <= right;
+        public static bool operator <(MoveScore left, int right)
+        {
+            return left.Score < right;
+        }
 
-        static bool IEqualityOperators<MoveScore, int, bool>.operator ==(MoveScore left, int right) => left.Score == right;
-        static bool IEqualityOperators<MoveScore, int, bool>.operator !=(MoveScore left, int right) => left.Score != right;
+        public static bool operator <=(MoveScore left, int right)
+        {
+            return left.Score <= right;
+        }
+
+        static bool IEqualityOperators<MoveScore, int, bool>.operator ==(MoveScore left, int right)
+        {
+            return left.Score == right;
+        }
+
+        static bool IEqualityOperators<MoveScore, int, bool>.operator !=(MoveScore left, int right)
+        {
+            return left.Score != right;
+        }
     }
 }
